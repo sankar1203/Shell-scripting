f2363f1af18ae0b3d245738bda16f7268b441a1c
sankar1203 <sankarvs1203@gmail.com>
2025-05-16 14:30:29 +0530

    Load the special variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,10 @@
 
 echo "Load all the variables in shell : $@"
 echo "no of variables passed: $#"
+echo "script name: $0"
+echo "present working directory : $PWD"
+echo "Home directory of the user who is running the script : $HOME"
+echo "Which user running the script : $USER"
+echo "PID of the current script : $$"
+sleep 10 &
+echo "PID of the last command runningin background: $!"
