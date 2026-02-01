f98f05dcc2333c53af867d1dc35cd47bb56ed0f2
sankar1203 <sankarvs1203@gmail.com>
2026-02-01 18:10:47 +0530

    commited first progrsm in shell

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -5,12 +5,12 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR: Please run this script with root access"
-    exit 1 #give  otherthan 0 upto 127
+    exit 1   #give  otherthan 0 upto 127
 else
     echo "you are running with root access"
 fi
 
-dnf install mysql -y
+dnf install mysqldgfgh -y
 
 if [ $? -eq 0 ]
 then
