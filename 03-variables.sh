a1bac878f03a945445f1e151af6245bdb1a7662b
sankar1203 <sankarvs1203@gmail.com>
2026-02-01 16:12:58 +0530

    commited first progrsm in shell

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ Person1=siva
 Person2=trump
 
 echo "$Person1:Hi trump This is a siva"
-echo "$Person2: hi this is Trump"
+echo "$Person2: hi this is trump"
 echo "$Person1: How are you"
-echo "$Person2: fine"
+echo "$Person2: fine siva"
--- 04-variables.sh
+++ 04-variables.sh
@@ -2,7 +2,7 @@
 
 Person1=$1
 Person2=$2
-echo "$Person1:Hi trump This is a siva"
-echo "$Person2: hi this is Trump"
+echo "$Person1:Hi $Person2 This is $Person1"
+echo "$Person2: hi this is $Person2"
 echo "$Person1: How are you"
-echo "$Person2: fine"
+echo "$Person2: fine $person1"
