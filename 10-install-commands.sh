0579370ace0c34427258bcc97b5555de2bcdecae
sankar1203 <sankarvs1203@gmail.com>
2026-02-01 17:44:25 +0530

    commited first progrsm in shell

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -4,9 +4,9 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0]
 then
-echo "ERROR: Please run this script with root access"
+    echo "ERROR: Please run this script with root access"
 else
-echo "you are running with root access"
+    echo "you are running with root access"
 fi
 
 dnf install mysql -y
