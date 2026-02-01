fb119d020e94fe0844ef7370803f803a27d590b7
sankar1203 <sankarvs1203@gmail.com>
2026-02-01 19:18:44 +0530

    commited first progrsm in shell

---
+++ 11-functions.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: Please run this script with root access"
+    exit 1   #give  otherthan 0 upto 127
+else
+    echo "you are running with root access"
+fi
+
+VALIDATE ()
+{
+    if [ $1 -eq 0 ]
+    then
+        echo "Installing $2 is.. success ..."
+    else
+        echo "Installing $2 is.. failure ..."
+    exit 1
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed ... Going to install on it"
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo "MySQL is already installed... nothing to do"
+    exit 1
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed ... Going to install on it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is already installed... nothing to do"
+    exit 1
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed ... Going to install on it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed... nothing to do"
+    exit 1
+fi
