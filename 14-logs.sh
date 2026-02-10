14ecee4cdc3a3f042350ecb3dbe6b0cacb14e492
sankar1203 <sankarvs1203@gmail.com>
2026-02-10 11:34:44 +0530

    Colors in shell scripting

--- 14-logs.sh
+++ 14-logs.sh
@@ -8,6 +8,12 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+LOGS_FOLDER=/var/log/shellscript-logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE= "$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script Started executing at: $date" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
